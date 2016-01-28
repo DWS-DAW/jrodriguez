d4d006447c548e891ccea5fc75ce286436c2a397
Jose Francisco RODRIGUEZ LLORENS <JFRODRIGUEZ@elecnor.es>
2016-01-28 14:20:42 +0100

    con index y casi acabada

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,7 +9,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+	
+	public function indexAction() {
+		return $this->render('default/app_index.html.twig');
+	}
+	
 	public function adminAction(){
 		return new Response('<html><body>Pagina del administrador!!</body></html>');
 	}
+	
+
 }
